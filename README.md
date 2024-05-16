a71c73d56eb4695ce58a3ab92960df7c8a3470e8
Mlie <emipa606@users.noreply.github.com>
2024-05-16 19:57:16 +0200

    Fixed an issue where the season-change is missed by the weather-replacer

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# SeasonalWeather
+# [Seasonal Weather (Continued)](https://steamcommunity.com/sharedfiles/filedetails/?id=2045114229)
 
 ![Image](https://i.imgur.com/buuPQel.png)
-
 Update of AaronCRobinsons mod
 https://github.com/AaronCRobinson/SeasonalWeather
 
 ![Image](https://i.imgur.com/pufA0kM.png)
-
 	
 ![Image](https://i.imgur.com/Z4GOv8H.png)
-
 
 
 SeasonalWeather
@@ -41,16 +38,14 @@ Special thanks to Pardeike's amazing non-destructive patching library, Harmony.
 ![Image](https://i.imgur.com/PwoNOj4.png)
 
 
-
 -  See if the the error persists if you just have this mod and its requirements active.
 -  If not, try adding your other mods until it happens again.
--  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib or the standalone https://steamcommunity.com/sharedfiles/filedetails/?id=2873415404]Uploader and command Ctrl+F12
+-  Post your error-log using [https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib[/url] or the standalone [url=https://steamcommunity.com/sharedfiles/filedetails/?id=2873415404](Uploader) and command Ctrl+F12
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
--  Use https://github.com/RimSort/RimSort/releases/latest]RimSort to sort your mods
+-  Use [https://github.com/RimSort/RimSort/releases/latest](RimSort) to sort your mods
 
 
 
-https://steamcommunity.com/sharedfiles/filedetails/changelog/2045114229]![Image](https://img.shields.io/github/v/release/emipa606/SeasonalWeather?label=latest%20version&style=plastic&color=9f1111&labelColor=black)
-
+[https://steamcommunity.com/sharedfiles/filedetails/changelog/2045114229]![Image]((https://img.shields.io/github/v/release/emipa606/SeasonalWeather?label=latest%20version&style=plastic&color=9f1111&labelColor=black))
--- Source/SeasonalWeather/SeasonalWeatherExtensionPatches.cs
+++ Source/SeasonalWeather/SeasonalWeatherExtensionPatches.cs
@@ -9,8 +9,6 @@ namespace SeasonalWeather;
 [StaticConstructorOnStartup]
 internal static class SeasonalWeatherExtensionPatches
 {
-    private const int TickerTypeLong = 2000;
-
     private static readonly MethodInfo MI_FindPlayerHomeWithMinTimezone =
         AccessTools.Method(typeof(DateNotifier), "FindPlayerHomeWithMinTimezone");
 
@@ -23,7 +21,8 @@ internal static class SeasonalWeatherExtensionPatches
         harmony.Patch(AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.FinalizeInit)),
             null, new HarmonyMethod(typeof(SeasonalWeatherExtensionPatches), nameof(FinalizeInit)));
         harmony.Patch(AccessTools.Method(typeof(DateNotifier), nameof(DateNotifier.DateNotifierTick)),
-            new HarmonyMethod(typeof(SeasonalWeatherExtensionPatches), nameof(DateNotifierTickPrefix)));
+            new HarmonyMethod(typeof(SeasonalWeatherExtensionPatches), nameof(DateNotifierTickPrefix)),
+            new HarmonyMethod(typeof(SeasonalWeatherExtensionPatches), nameof(DateNotifierTickPostfix)));
     }
 
     // NOTE: should this be somewhere else?
@@ -32,42 +31,41 @@ internal static class SeasonalWeatherExtensionPatches
         CheckBaseWeatherCommonalities(Find.DateNotifier);
     }
 
-    public static void DateNotifierTickPrefix(DateNotifier __instance)
+    public static void DateNotifierTickPrefix(DateNotifier __instance, out Season __state)
     {
-        if (__instance.IsHashIntervalTick(TickerTypeLong))
+        __state = (Season)FI_lastSeason.GetValue(__instance);
+    }
+
+    public static void DateNotifierTickPostfix(DateNotifier __instance, Season __state)
+    {
+        var currentLastSeason = (Season)FI_lastSeason.GetValue(__instance);
+        if (currentLastSeason == __state)
         {
-            CheckBaseWeatherCommonalities(__instance);
+            return;
         }
+
+        CheckBaseWeatherCommonalities(__instance);
     }
 
     private static void CheckBaseWeatherCommonalities(DateNotifier __instance)
     {
         var map = (Map)MI_FindPlayerHomeWithMinTimezone.Invoke(__instance, []);
-        if (map != null)
-        {
-            var ext = map.Biome.GetModExtension<SeasonalWeatherExtension>();
-            if (ext != null)
-            {
-                var season = map.GetSeason();
-                var lastSeason = (Season)FI_lastSeason.GetValue(__instance);
-                if (season == lastSeason ||
-                    lastSeason != Season.Undefined && season == lastSeason.GetPreviousSeason())
-                {
-                    return;
-                }
-
-                Log.Message("SeasonalWeather: season changed");
-                ext.AdjustBaseWeatherCommonalities(map, season);
-            }
-            else
-            {
-                LogUtility.MessageOnce("Custom biome does not have Seasonal Weather data.", 725491);
-            }
-        }
-        else
+        if (map == null)
         {
             LogUtility.MessageOnce("No map found to check base weather commonalities? NomadsLand?", 8720412);
+            return;
         }
+
+        var ext = map.Biome.GetModExtension<SeasonalWeatherExtension>();
+        if (ext == null)
+        {
+            LogUtility.MessageOnce("Custom biome does not have Seasonal Weather data.", map.Biome.GetHashCode());
+            return;
+        }
+
+
+        Log.Message("SeasonalWeather: season changed");
+        ext.AdjustBaseWeatherCommonalities(map, map.GetSeason());
     }
 
     private static Season GetSeason(this Map map)
